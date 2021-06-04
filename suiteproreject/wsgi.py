5080c42afcac14dd41487cf3e15354a1443d0dfd
T-Phage <samuelgyamfi824@gmail.com>
2021-06-04 02:00:09 +0000

    first commit

--- suiteproreject/wsgi.py
+++ suiteproreject/wsgi.py
@@ -8,11 +8,11 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-# from whitenoise import WhiteNoise
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'suiteproreject.settings')
 
 application = get_wsgi_application()
-# application = WhiteNoise(application, root='/path/to/static/files')
-# application.add_files('/static/', prefix='more-files/')
+application = WhiteNoise(application, root='/path/to/static/files')
+application.add_files('/static/', prefix='more-files/')
